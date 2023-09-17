a4b52db77d7947f45b4b20b5b75bfe7f5ae48c9e
Parth <parthzarekar@gmail.com>
2023-09-18 02:02:12 +0530

    frontend complet

---
+++ Server/index.js
@@ -0,0 +1,27 @@
+import app from "./server.js";
+import mongodb from "mongodb";
+// import ReviewsDAO from "./dao/reviewsDAO.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const MongoClient = mongodb.MongoClient;
+const mongo_username = process.env["MONGO_USERNAME"];
+const mongo_password = process.env["MONGO_PASSWORD"];
+const db_name = process.env["DB_NAME"];
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@cluster0.kbl8ceb.mongodb.net/${db_name}?retryWrites=true&w=majority`;
+
+const port = process.env["PORT"];
+
+MongoClient.connect(uri, {
+  maxPoolSize: 50, // no.of connetion allowed
+  wtimeoutMS: 2500, // time to wait for connection
+  useNewUrlParser: true, // new url parser
+}).catch(err =>{
+    console.error(err.stack)
+    process.exit(1)
+})
+.then(async client => {
+    app.listen(port,() => {
+        console.log(`listening on port ${port}`)
+    })
+})
